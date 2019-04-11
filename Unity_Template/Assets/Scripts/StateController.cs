ecb36e29cf466dd3b82ba11e94fb2ac82a7cd457
Jhburns <jburns@chapman.edu>
2019-04-11 16:34:43 -0700

    All states for serlized objects are now tracked

--- Unity_Template/Assets/Scripts/StateController.cs
+++ Unity_Template/Assets/Scripts/StateController.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 using System.Linq;
 using Serial;
 
+
 public class StateController : MonoBehaviour
 {
+    Serializable[] allSerialObjects;
+    Stack<SerialDataStore[]> pastStates;
+
     void Start()
     {
-        var allSerialObjects = FindObjectsOfType<MonoBehaviour>().OfType<Serializable>();
-        foreach (Serializable s in allSerialObjects)
-        {
-            Debug.Log(s);
-        }
+        var serialQuery = FindObjectsOfType<MonoBehaviour>().OfType<Serializable>();
+        allSerialObjects = serialQuery.Cast<Serializable>().ToArray();
+
+        pastStates = new Stack<SerialDataStore[]>();
     }
 
     void Update()
     {
+        pastStates.Push(collectStates());
+    }
 
+    SerialDataStore[] collectStates()
+    {
+        SerialDataStore[] allCurrentStates = new SerialDataStore[allSerialObjects.Length];
+
+        for (int i = 0; i < allSerialObjects.Length; i++)
+        {
+            allCurrentStates[i] = allSerialObjects[i].GetCurrentState();
+        }
+
+        return allCurrentStates;
     }
 }
