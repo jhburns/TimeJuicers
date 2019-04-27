312a715d4d74e7af47b9d948cc60feb2c62adfe4
Jhburns <jburns@chapman.edu>
2019-04-27 04:21:33 -0700

    Fixing player death

--- Unity_Template/Assets/Scripts/Player.cs
+++ Unity_Template/Assets/Scripts/Player.cs
@@ -228,8 +228,9 @@ public class Player : MonoBehaviour, ISerializable
             }
         }
 
-        if (col.gameObject.GetComponent<Enemy>() != null || col.gameObject.name == "DeathZone" && deathHandler.IsAlive) // Can only die once
+        if ((col.gameObject.GetComponent<Enemy>() != null || col.gameObject.name == "DeathZone") && deathHandler.IsAlive) // Can only die once
         {
+            Debug.Log("Died");
             deathHandler.OnDeath();
          }
     }
