d34412f02f6911e37bd3a0e4ae2cda65f7ce9705
Haifan Chen <meiguo1969@gmail.com>
2019-05-13 00:18:51 -0700

    add background music

---
+++ Unity_Template/Assets/Scripts/GameMusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+//https://answers.unity.com/questions/1260393/make-music-continue-playing-through-scenes.html
+
+public class GameMusicPlayer : MonoBehaviour
+{
+
+    private static GameMusicPlayer instance = null;
+    public static GameMusicPlayer Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+
