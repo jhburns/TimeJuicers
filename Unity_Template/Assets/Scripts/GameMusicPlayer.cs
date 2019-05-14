7fe7176ab65538f940b42acfae90830f9bf6463a
Jhburns <jburns@chapman.edu>
2019-05-13 23:05:37 -0700

    More clouds in the main menu so they aren't as easily seen at looping

--- Unity_Template/Assets/Scripts/GameMusicPlayer.cs
+++ Unity_Template/Assets/Scripts/GameMusicPlayer.cs
@@ -5,11 +5,13 @@ using System.Collections;
 public class GameMusicPlayer : MonoBehaviour
 {
 
-    private static GameMusicPlayer instance = null;
+    private static GameMusicPlayer instance;
+
     public static GameMusicPlayer Instance
     {
         get { return instance; }
     }
+
     void Awake()
     {
         if (instance != null && instance != this)
